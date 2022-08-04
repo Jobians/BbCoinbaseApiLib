e5ee3f4be756f500af815ebf6a020e259590e97a
Jobians Techie <88005779+Jobians@users.noreply.github.com>
2022-08-04 21:19:46 +0100

    Update _tx.js

--- commands/Withdraw/_tx.js
+++ commands/Withdraw/_tx.js
@@ -17,12 +17,6 @@ CMD*/
 
 if(!params){return}
 
-Api.answerCallbackQuery({
-  callback_query_id: request.id,
-  text: "ℹ️ Getting transaction info...",
-  show_alert: false // or false - for alert on top
-})
-
 var id = params.split(" ")[0]
 var coin = params.split(" ")[1]
 
